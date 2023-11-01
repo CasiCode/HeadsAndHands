7ea373ed8dd19095a7fa4d3ccc7802c3867eb804
casi <pitsunovtimur@gmail.com>
2023-11-01 19:17:20 +0400

    fixes for commented mistakes

--- Lesson_03_Pitsunov/app/src/main/java/com/lesson_03_pitsunov/LayoutActivity.kt
+++ Lesson_03_Pitsunov/app/src/main/java/com/lesson_03_pitsunov/LayoutActivity.kt
@@ -7,9 +7,7 @@ import android.widget.ImageButton
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.appbar.MaterialToolbar
 
 class LayoutActivity : AppCompatActivity() {
@@ -19,46 +17,46 @@ class LayoutActivity : AppCompatActivity() {
 
         val user: User = User()
         findViewById<TextView>(R.id.cardNumberTextView).text = buildString {
-            append("Карта №")
+            append(getString(R.string.card_number_title))
             append(user.cardNumber)
         }
         findViewById<TextView>(R.id.roleTextView).text = user.role
 
         val nameBlock: RelativeLayout = findViewById(R.id.nameTextBlock)
-        findViewById<TextView>(R.id.titleTextView).text = "Имя"
+        findViewById<TextView>(R.id.titleTextView).text = getString(R.string.name_title)
         nameBlock.findViewById<TextView>(R.id.contentTextView).text = user.name
 
         val surnameBlock: RelativeLayout = findViewById(R.id.surnameTextBlock)
-        surnameBlock.findViewById<TextView>(R.id.titleTextView).text = "Фамилия"
+        surnameBlock.findViewById<TextView>(R.id.titleTextView).text = getString(R.string.surname_title)
         surnameBlock.findViewById<TextView>(R.id.contentTextView).text = user.surname
 
         val emailBlock: RelativeLayout = findViewById(R.id.emailTextBlock)
-        emailBlock.findViewById<TextView>(R.id.titleTextView).text = "E-mail"
+        emailBlock.findViewById<TextView>(R.id.titleTextView).text = getString(R.string.email_title)
         emailBlock.findViewById<TextView>(R.id.contentTextView).text = user.email
 
         val loginBlock: RelativeLayout = findViewById(R.id.loginTextBlock)
-        loginBlock.findViewById<TextView>(R.id.titleTextView).text = "Логин"
+        loginBlock.findViewById<TextView>(R.id.titleTextView).text = getString(R.string.login_title)
         loginBlock.findViewById<TextView>(R.id.contentTextView).text = user.login
 
         val regionBlock: RelativeLayout = findViewById(R.id.regionTextBlock)
-        regionBlock.findViewById<TextView>(R.id.titleTextView).text = "Ваш регион"
+        regionBlock.findViewById<TextView>(R.id.titleTextView).text = getString(R.string.region_title)
         regionBlock.findViewById<TextView>(R.id.contentTextView).text = user.region
         regionBlock.findViewById<ImageButton>(R.id.textViewBlockButton).visibility = VISIBLE
         regionBlock.findViewById<ImageButton>(R.id.textViewBlockButton).setOnClickListener {
-            Toast.makeText(this, "Нельзя сбежать из Саратова", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.saratov_toast_text), Toast.LENGTH_SHORT).show()
         }
 
         findViewById<MaterialToolbar>(R.id.toolbar).setNavigationOnClickListener {
-            Toast.makeText(this, "Стрелочка", Toast.LENGTH_SHORT).show()
+            finish()
         }
 
         findViewById<MaterialToolbar>(R.id.toolbar).setOnMenuItemClickListener {
-            Toast.makeText(this, "Карандашик", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.pencil_button_toast_text), Toast.LENGTH_SHORT).show()
             true
         }
 
         findViewById<Button>(R.id.logoutButton).setOnClickListener {
-            Toast.makeText(this, "НЕ УХАДИ", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.dont_leave_toast_text), Toast.LENGTH_SHORT).show()
         }
     }
 }
